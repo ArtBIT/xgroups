bdf659f58cb90d2398ab1688bc08534eebf2f12f
Djordje Ungar <djordje.ungar@gmail.com>
2025-04-23 15:57:10 +0200

    Update updateURL

--- tampermonkey-header.js
+++ tampermonkey-header.js
@@ -6,8 +6,8 @@
 // @copyright    2025+ Djordje Ungar
 // @version      1.0.0
 // @source       https://github.com/artbit/xgroups
-// @updateURL    https://raw.githubusercontent.com/artbit/xgroups/main/dist/xgroups.userscript.js
-// @downloadURL  https://raw.githubusercontent.com/artbit/xgroups/main/dist/xgroups.userscript.js
+// @updateURL    https://raw.githubusercontent.com/ArtBIT/xgroups/refs/heads/main/dist/xgroups.userscript.js
+// @downloadURL  https://raw.githubusercontent.com/ArtBIT/xgroups/refs/heads/main/dist/xgroups.userscript.js
 // @match        https://x.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=x.com
 // @tag          productivity
