eab5bd1baf14639f117f41fa762052b4414f4107
Djordje Ungar <djordje.ungar@gmail.com>
2025-04-25 12:40:23 +0200

    Rebuild

--- dist/xgroups.meta.js
+++ dist/xgroups.meta.js
@@ -5,7 +5,7 @@
 // @author       Djordje Ungar
 // @copyright    2025+ Djordje Ungar
 // @license      MIT
-// @version      1.0.87
+// @version      1.0.132
 // @source       https://github.com/artbit/xgroups
 // @homepageURL  https://github.com/artbit/xgroups
 // @supportURL   https://github.com/artbit/xgroups/issues
